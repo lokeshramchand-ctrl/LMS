5cbff785039ab5b1892a60a0f74c64d61cc7b497
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-02-24 21:33:36 +0530

    9.33

---
+++ front/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Register from "./Components/Register/register.jsx";
+
+function App() {
+
+  return (
+    <>
+      <Register />
+
+    </>
+  )
+}
+
+export default App
