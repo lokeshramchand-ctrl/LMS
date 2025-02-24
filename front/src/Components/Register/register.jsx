bff1eb7d6ab9263325a767efdbbc45c8d1efbba5
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-02-25 04:49:35 +0530

    4.49

--- front/src/Components/Register/register.jsx
+++ front/src/Components/Register/register.jsx
@@ -4,36 +4,29 @@ import "./Register.css";
 const Register = () => {
   return (
     <div className="Entire">
-    <div className="image-container">
-      <div className="register-image">
-        <img src="./src/assets/1.png" alt="Register" />
+      <div className="image-container">
+        <div className="register-image">
+          <img src="./src/assets/1.png" alt="Register" />
+        </div>
       </div>
-    </div>
-    <div className="right-container">
-<div class="input__container">
-  <div class="shadow__input"></div>
-  <button class="input__button__shadow">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="#000000"
-      width="20px"
-      height="20px"
-    >
-      <path d="M0 0h24v24H0z" fill="none"></path>
-      <path
-        d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
-      ></path>
-    </svg>
-  </button>
-  <input
-    type="text"
-    name="username"
-    class="input__search"
-    placeholder="Enter username"
-  />
-</div>
-</div>
+      <div className="right-container">
+        <div class="input__container">
+          <input
+            type="text"
+            name="username"
+            class="input__search"
+            placeholder="Enter username"
+          />
+        </div>
+        <div class="input_container">
+          <input
+            type="text"
+            name="username"
+            class="input_search"
+            placeholder="Enter username"
+          />
+        </div>
+      </div>
     </div>
   );
 };
