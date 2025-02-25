3c26501821ac3d0e73af9cfd9eab57152b37604a
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-02-25 22:19:55 +0530

    10.04

--- front/src/Components/test/teste.jsx
+++ front/src/Components/test/teste.jsx
@@ -102,26 +102,21 @@ const Teste = () => {
               required
             />
           </div>
-          <button type="submit">
-            <div class="bgContainer">
-              <span>Hover</span>
-              <span>Hover</span>
-            </div>
-            <div class="arrowContainer">
-              <svg
-                width="25"
-                height="25"
-                viewBox="0 0 45 38"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M43.7678 20.7678C44.7441 19.7915 44.7441 18.2085 43.7678 17.2322L27.8579 1.32233C26.8816 0.34602 25.2986 0.34602 24.3223 1.32233C23.346 2.29864 23.346 3.88155 24.3223 4.85786L38.4645 19L24.3223 33.1421C23.346 34.1184 23.346 35.7014 24.3223 36.6777C25.2986 37.654 26.8816 37.654 27.8579 36.6777L43.7678 20.7678ZM0 21.5L42 21.5V16.5L0 16.5L0 21.5Z"
-                  fill="black"
-                ></path>
-              </svg>
-            </div>
-          </button>
+<button class="animated-button">
+  <svg viewBox="0 0 24 24" class="arr-2" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+    ></path>
+  </svg>
+  <span class="text">Login</span>
+  <span class="circle"></span>
+  <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+    ></path>
+  </svg>
+</button>
+
         </div>
       </form>
     </div>
